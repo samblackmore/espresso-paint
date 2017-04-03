beca8facc609b08d5ccf831b517f04d0191570b6
samblackmore <samblackmore07@gmail.com>
2017-04-03 01:22:29 -0400

    adding and populating sidebar

--- main.js
+++ main.js
@@ -5,9 +5,19 @@ Object.prototype.hasOwnProperties = function(arr) {
   });
 }
 
+HTMLSelectElement.prototype.addOption = function(text) {
+  if (this.type === 'select-one') {
+    var option = document.createElement('option');
+    var txt = document.createTextNode(text);
+    option.appendChild(txt);
+    this.appendChild(option);
+  }
+}
+
 var fontSize = 20;
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var filters = {};
 
 function submit() {
   var nodeRegex = /\+-*>.+}/g
@@ -34,6 +44,10 @@ function submit() {
         // Do nothing
       }
       obj[key] = val;
+      if (!filters.hasOwnProperty(key))
+        filters[key] = [];
+      if (filters[key].indexOf(val) === -1)
+        filters[key].push(val)
     }
     console.log(name);
     console.log(obj);
@@ -54,5 +68,23 @@ function submit() {
       ctx.font = fontSize*0.7 + 'px monospace';
       ctx.fillText(obj['res-name'], obj.x, obj.y - 5);
     }
+  }
+  console.log('Filters:');
+  console.log(filters);
+  var sidebar = document.getElementById('filters');
+  sidebar.innerHtml = '';
+  for (var filter in filters) {
+    var dropdown = document.createElement('select');
+    dropdown.className = 'dropdown';
+    dropdown.addOption('any');
+    var options = filters[filter];
+    for (var i = 0; i < options.length; i++) {
+      dropdown.addOption(options[i]);
+    }
+    var name = document.createTextNode(filter);
+    sidebar.appendChild(name);
+    sidebar.appendChild(document.createElement('br'));
+    sidebar.appendChild(dropdown);
+    sidebar.appendChild(document.createElement('br'));
   }
 }
